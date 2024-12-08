aecd09db72bbfed3038c6104b7778586d4659ca3
Bedram Tamang <tmgbedu@gmail.com>
2024-12-07 21:58:17 -0800

    feat: ts fix

--- hiive-frontend/src/pages/posts/PostCreate.tsx
+++ hiive-frontend/src/pages/posts/PostCreate.tsx
@@ -1,11 +1,12 @@
 import axios, { AxiosError } from "axios"
 import React, { useState } from "react"
+import { Error } from "../../components/form/Error.tsx"
 
 export function PostCreate() {
     const [formData, setFormData] = useState({ title: "", description: "" })
-    const [errors, setErrors] = useState({})
+    const [errors, setErrors] = useState<{ [key: string]: string }>({})
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement|HTMLTextAreaElement>) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target
         setFormData((prev) => ({ ...prev, [name]: value }))
     }
@@ -13,18 +14,17 @@ export function PostCreate() {
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         try {
-            const response = await axios.post("http://localhost:4000/api/posts", formData, {
+            await axios.post("http://localhost:4000/api/posts", formData, {
                 withCredentials: true,
                 headers: {
                     "Content-Type": "application/json",
                 },
             })
-            console.log("Response:", response.data)
         } catch (error) {
-            if (error.response.status === 422) {
-                let errors:  { [key: string]: string }  = {}
+            if (error instanceof AxiosError && error?.response?.status === 422) {
+                let errors: { [key: string]: string } = {}
                 Object.keys(error.response.data.errors).forEach((item: string) => {
-                    errors[item] = error.response.data.errors[item][0]
+                    errors[item] = error.response?.data.errors[item][0]
                 })
 
                 setErrors(errors)
@@ -44,6 +44,7 @@ export function PostCreate() {
                                 value={formData.title}
                                 onChange={handleChange}
                                 className="border rounded w-full px-2 py-1"/>
+                            <Error message={errors["title"]}/>
                         </div>
                         <div className="mb-4">
                             <label>Description:</label>
@@ -52,6 +53,7 @@ export function PostCreate() {
                                 value={formData.description}
                                 onChange={handleChange}
                                 className="border w-full rounded px-2 py-1"/>
+                            <Error message={errors["title"]}/>
                         </div>
                         <div className="flex justify-end">
                             <button type="submit" className="rounded bg-green-500 px-4 py-2">Create</button>
